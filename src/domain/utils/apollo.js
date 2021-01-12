1bafe38c14bb23b84c0292f6c2fac30523687bb3
Rupa Dhillon <rupa211984@gmail.com>
2021-01-12 03:44:55 -0800

    Add new key

--- src/domain/utils/apollo.js
+++ src/domain/utils/apollo.js
@@ -4,7 +4,7 @@ import { GITHUB_ENDPOINT, REQUEST_TIMEOUT } from "../../constants/app";
 import ApolloLinkTimeout from "apollo-link-timeout";
 
 const httpLink = new HttpLink({ uri: GITHUB_ENDPOINT });
-const token = "d863f509a5a61e4a508e91771fafcc004f59cb14";
+const token = "8a09b348e3b4ca491ed941e09f105cb14d32e479";
 
 const authLink = apolloUtils.authLink({
   authorization: token ? `Bearer ${token}` : "",
